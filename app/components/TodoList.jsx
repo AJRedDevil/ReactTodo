bcd780102691a2abbdeb2390c893fd16bb61bf12
Ajan Lal Shrestha <ajan.shrestha@logpoint.com>
2017-07-04 10:58:38 +0545

    Refactor app to work with local storage

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -1,7 +1,7 @@
 var React = require('react');
 var {connect} = require('react-redux');
 import Todo from 'Todo';
-var TodoAPI = require('TodoApi');
+var TodoAPI = require('TodoAPI');
 
 export var TodoList = React.createClass({
     render: function () {
